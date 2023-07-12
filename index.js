8f4b12f78a72907387b3c27cd00842a0a1be571d
Johora-Juhi <zohrajuhi1012@gmail.com>
2023-07-12 22:27:36 +0600

    get profile info

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const jwt = require("jsonwebtoken");
-// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
@@ -13,16 +12,6 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bkdzfxe.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -54,7 +43,7 @@ async function run() {
     const interestedCustomerCollection = client
       .db("hairSalon")
       .collection("interestedCustomer");
-      const ordersCollection = client.db("hairSalon").collection("orders");
+    const ordersCollection = client.db("hairSalon").collection("orders");
 
     // VERIFY ADMIN
     const verifyAdmin = async (req, res, next) => {
@@ -135,6 +124,14 @@ async function run() {
         options
       );
       res.send(result);
+    });
+
+    // get user
+    app.get("/profile/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await usersCollection.find(query).toArray();
+      res.send(user);
     });
 
     //get all blogs
@@ -220,7 +217,7 @@ async function run() {
       res.send(users);
     });
 
-    // place order 
+    // place order
     app.post("/orderPlace", verifyJWT, async (req, res) => {
       const orderPlace = req.body;
       console.log(orderPlace);
@@ -228,8 +225,6 @@ async function run() {
       res.send(result);
     });
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
 run().catch(console.dir);
